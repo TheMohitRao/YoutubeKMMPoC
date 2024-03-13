dee7a333d53b2f9135f37babebbfde0db9a0a21d
mohit <mohit.yadav@alerzo.com>
2024-03-13 11:25:55 +0530

    Android and Desktop Support added

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,10 +1,20 @@
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    
+    alias(libs.plugins.androidLibrary)
 }
 
 kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "11"
+            }
+        }
+    }
+    
+    jvm()
+    
     listOf(
         iosX64(),
         iosArm64(),
@@ -23,3 +33,14 @@ kotlin {
     }
 }
 
+android {
+    namespace = "com.themohitrao.pyp.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
